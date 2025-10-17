10737c4508f546adc62304e07e47f3148987699d
Carlos E-Ribeiro <carlosesilva466@gmail.com>
2025-10-17 17:02:41 -0300

    atualizando jenkinsfile

--- src/test/java/programa/Exlog4j.java
+++ src/test/java/programa/Exlog4j.java
@@ -3,7 +3,7 @@ package programa;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-public class ExLog4j {
+public class Exlog4j {
     private static final Logger LOG = LogManager.getLogger(ExLog4j.class);
     public static void main(String[] args) {
         LOG.info("hello from log4j2");
