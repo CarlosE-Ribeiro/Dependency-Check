391e366cac0a68adf3d7a870cb1dc8a90efccc4d
Carlos E-Ribeiro <carlosesilva466@gmail.com>
2025-10-24 00:49:51 -0300

    subindo teste com dependencia

---
+++ src/main/java/programa/Teste.java
@@ -0,0 +1,18 @@
+package programa;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Teste {
+    private static final Logger logger = LogManager.getLogger(Teste.class);
+
+    public static void main(String[] args) {
+        System.out.println("Iniciando aplicação de teste de vulnerabilidade...");
+
+        // Usando o logger para garantir que a dependência seja carregada.
+        // O código abaixo, em um ambiente vulnerável, poderia ser explorado.
+        logger.error("Este é um teste de log com uma dependência vulnerável.");
+
+        System.out.println("Aplicação finalizada.");
+    }
+}
